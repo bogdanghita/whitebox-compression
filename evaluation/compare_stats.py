ba5138dad82a3c4476216fa6d35369824bab04ea
Bogdan Ghita <bogdan.ghita93@gmail.com>
2019-05-13 15:00:51 +0200

    [compare_stats] agg used columns stats

--- evaluation/compare_stats.py
+++ evaluation/compare_stats.py
@@ -32,7 +32,7 @@ def compare_data_files(s_file1, s_file2, s_data1, s_data2):
 	return output
 
 
-def compare_columns(s_file1, s_file2, s_data1, s_data2, expr_tree_file, apply_expr_stats_file):
+def compare_ccs(s_file1, s_file2, s_data1, s_data2, expr_tree_file, apply_expr_stats_file):
 	column_data = {
 		s_file1: {c["col_data"]["col_name"]: c for c in s_data1["columns"].values()},
 		s_file2: {c["col_data"]["col_name"]: c for c in s_data2["columns"].values()}
@@ -50,19 +50,22 @@ def compare_columns(s_file1, s_file2, s_data1, s_data2, expr_tree_file, apply_ex
 		# print(json.dumps(apply_expr_stats, indent=2))
 
 	ccs = expr_tree.get_connected_components()
-	for cc in ccs:
-		print("levels={}, in_columns={}".format(cc.levels, cc.get_in_columns()))
+	# for cc in ccs:
+	# 	print("levels={}, in_columns={}".format(cc.levels, cc.get_in_columns()))
 
-	output = "\n*** data_files (column level) ***"
+	agg_output = "\n*** data files (aggregated; used columns) ***"
+	ccs_output = "\n*** data_files (connected components) ***"
 
 	if expr_tree:
+		agg_in_size_B, agg_total_out_size_B = 0, 0
+
 		for cc_expr_tree in expr_tree.get_connected_components():
 			in_columns = [cc_expr_tree.get_column(col_id) for col_id in cc_expr_tree.get_in_columns()]
 			out_columns = [cc_expr_tree.get_column(col_id) for col_id in cc_expr_tree.get_out_columns()]
 
 			# in_cols
 			in_size_B = 0
-			output += "\n\n[input_colums]"
+			ccs_output += "\n\n[input_colums]"
 			for col_item in in_columns:
 				in_col = col_item["col_info"]
 				col_id, col_name = in_col.col_id, in_col.name
@@ -70,11 +73,11 @@ def compare_columns(s_file1, s_file2, s_data1, s_data2, expr_tree_file, apply_ex
 					raise Exception("error: in_col_name={} not found in s_file1={}".format(col_name, s_file1))
 				col_size_B = column_data[s_file1][col_name]["data_files"]["data_file"]["size_B"]
 				in_size_B += col_size_B
-				output += "\ncol_id={}, col_name={}, size={}, in_col={}".format(in_col.col_id, col_name, sizeof_fmt(col_size_B), in_col)
+				ccs_output += "\ncol_id={}, col_name={}, size={}, in_col={}".format(in_col.col_id, col_name, sizeof_fmt(col_size_B), in_col)
 
 			# out_cols
 			out_size_B = 0
-			output += "\n[output_colums]"
+			ccs_output += "\n[output_colums]"
 			for col_item in out_columns:
 				out_col = col_item["col_info"]
 				# handle expcetion columns separately
@@ -89,11 +92,11 @@ def compare_columns(s_file1, s_file2, s_data1, s_data2, expr_tree_file, apply_ex
 					null_ratio = "N/A"
 				col_size_B = column_data[s_file2][col_name]["data_files"]["data_file"]["size_B"]
 				out_size_B += col_size_B
-				output += "\ncol_id={}, col_name={}, null_ratio={}, size={}, out_col={}".format(out_col.col_id, col_name, null_ratio, sizeof_fmt(col_size_B), out_col)
+				ccs_output += "\ncol_id={}, col_name={}, null_ratio={}, size={}, out_col={}".format(out_col.col_id, col_name, null_ratio, sizeof_fmt(col_size_B), out_col)
 
 			# exception columns
 			ex_size_B = 0
-			output += "\n[exception_colums]"
+			ccs_output += "\n[exception_colums]"
 			for col_item in out_columns:
 				ex_col = col_item["col_info"]
 				if not ExceptionColumnManager.is_exception_col(ex_col):
@@ -107,13 +110,13 @@ def compare_columns(s_file1, s_file2, s_data1, s_data2, expr_tree_file, apply_ex
 					null_ratio = "N/A"
 				col_size_B = column_data[s_file2][col_name]["data_files"]["data_file"]["size_B"]
 				ex_size_B += col_size_B
-				output += "\ncol_id={}, col_name={}, null_ratio={}, size={}, ex_col={}".format(ex_col.col_id, col_name, null_ratio, sizeof_fmt(col_size_B), ex_col)
+				ccs_output += "\ncol_id={}, col_name={}, null_ratio={}, size={}, ex_col={}".format(ex_col.col_id, col_name, null_ratio, sizeof_fmt(col_size_B), ex_col)
 
 			# summary
 			total_out_size_B = out_size_B + ex_size_B
 			compression_ratio = float(in_size_B) / total_out_size_B if total_out_size_B > 0 else float("inf")
-			output += "\n[summary] {}".format([(idx, cc_expr_tree.get_node(n_id).p_id) for idx, l in enumerate(cc_expr_tree.levels) for n_id in l])
-			output += "\ncc={cc}, compression_ratio={compression_ratio:.2f}, in_size_B={in_size}, total_out_size_B={total_out_size} (out_size={out_size}, ex_size={ex_size})".format(
+			ccs_output += "\n[summary] {}".format([(idx, cc_expr_tree.get_node(n_id).p_id) for idx, l in enumerate(cc_expr_tree.levels) for n_id in l])
+			ccs_output += "\ncc={cc}, compression_ratio={compression_ratio:.2f}, in_size_B={in_size}, total_out_size_B={total_out_size} (out_size={out_size}, ex_size={ex_size})".format(
 					cc=cc_expr_tree.get_in_columns(),
 					compression_ratio=compression_ratio,
 					in_size=sizeof_fmt(in_size_B),
@@ -122,6 +125,17 @@ def compare_columns(s_file1, s_file2, s_data1, s_data2, expr_tree_file, apply_ex
 					ex_size=sizeof_fmt(ex_size_B)
 				)
 
+			agg_in_size_B += in_size_B
+			agg_total_out_size_B += total_out_size_B
+
+		# aggregate for used columns
+		agg_compression_ratio = float(agg_in_size_B) / agg_total_out_size_B if agg_total_out_size_B > 0 else float("inf")
+		agg_output += "\nagg_in_size_B={agg_in_size_B}, agg_total_out_size_B={agg_total_out_size_B}, agg_compression_ratio={agg_compression_ratio:.2f}".format(
+					agg_in_size_B=sizeof_fmt(agg_in_size_B),
+					agg_total_out_size_B=sizeof_fmt(agg_total_out_size_B),
+					agg_compression_ratio=agg_compression_ratio)
+
+	output = agg_output + "\n" + ccs_output
 	return output
 
 
@@ -139,10 +153,10 @@ def compare_stats(s_file1, s_file2, expr_tree_file, apply_expr_stats_file):
 
 	# expression nodes: in/out column comparison
 	try:
-		output_cols = compare_columns(s_file1, s_file2, s_data1, s_data2, expr_tree_file, apply_expr_stats_file)
+		output_cols = compare_ccs(s_file1, s_file2, s_data1, s_data2, expr_tree_file, apply_expr_stats_file)
 		print(output_cols)
 	except Exception as e:
-		print("error: unable to perform: compare_columns; e={}".format(e))
+		print("error: unable to perform: compare_ccs; e={}".format(e))
 
 
 def parse_args():
